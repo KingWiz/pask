8313a91c4282e7d255a2b3703f866e73dc7cddbc
anjanshrestha8 <ghalepasang5@gmail.com>
2022-05-25 08:17:35 +0545

    frontend changed

---
+++ DMS/DMS.UI/Controllers/InboxController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DMS.Controllers
+{
+    [AllowAnonymous]
+    public class InboxController : Controller
+    {
+        // GET: Inbox
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ paskQuery.sql
@@ -0,0 +1,2 @@
+create database pask;
+use pask;
